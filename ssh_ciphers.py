7a32025328947f1248adf75a3c994f32c343a09f
Andy Smith <andy@ajsmith.us>
2019-11-05 13:57:13 -0500

    Improve ssh ciphers exception handling

--- ssh_ciphers.py
+++ ssh_ciphers.py
@@ -5,11 +5,28 @@ class SSHCiphers:
 	def __init__(self, ip_address: str, port: int):
 		self.ip_address = ip_address
 		self.port = port
+		
+		self.weak_ciphers = []
+		self.weak_macs = []
+		self.weak_kex = []
+		self.weak_hka = []
+		self.ciphers = []
+		self.macs = []
+		self.kex = []
+		self.hka = []
+
 		self.__connect()
 		self.__parse_ciphers()
 		
 	def __connect(self):
-		conn = socket.create_connection((self.ip_address, self.port),5)
+		self.raw_ciphers = None
+		self.raw_banner = None
+
+		try:
+			conn = socket.create_connection((self.ip_address, self.port),5)
+		except (ConnectionRefusedError, ConnectionResetError) as err:
+			return
+
 		conn.settimeout(2.0)
 		banner = conn.recv(50).split(b'\n')[0]
 		conn.send(b'SSH-2.0-OpenSSH_7.9p1\r\n')
@@ -19,13 +36,10 @@ class SSHCiphers:
 		self.raw_banner = banner
 
 	def __parse_ciphers(self):
-		self.weak_ciphers = []
-		self.weak_macs = []
-		self.weak_kex = []
-		self.weak_hka = []
+		# Skip parse if connection was unsuccessful
+		if self.raw_ciphers == None or self.raw_banner == None: return
 		
 		# Message authentication code algorithms
-		self.macs = []
 		for alg in Algorithms.ALGORITHMS["mac"].keys():
 			if alg in self.raw_ciphers:
 				self.macs.append(alg)
@@ -33,7 +47,6 @@ class SSHCiphers:
 					self.weak_macs.append(alg)
 		
 		# Ciphers
-		self.ciphers = []
 		for alg in Algorithms.ALGORITHMS["enc"].keys():
 			if alg in self.raw_ciphers:
 				self.ciphers.append(alg)
@@ -41,7 +54,6 @@ class SSHCiphers:
 					self.weak_ciphers.append(alg)
 		
 		# Key exchange algorithms
-		self.kex = []
 		for alg in Algorithms.ALGORITHMS["kex"].keys():
 			if alg in self.raw_ciphers:
 				self.kex.append(alg)
@@ -49,7 +61,6 @@ class SSHCiphers:
 					self.weak_kex.append(alg)
 		
 		# Host key algorithms
-		self.hka = []
 		for alg in Algorithms.ALGORITHMS["key"].keys():
 			if alg in self.raw_ciphers:
 				self.hka.append(alg)
