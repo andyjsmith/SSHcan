dbda16e83b1f9d7c8321d4c42a3f8b0723fe6641
Andy Smith <andy@ajsmith.us>
2019-11-05 13:09:32 -0500

    Add progress bar to host resolver

--- ssh_host_resolver.py
+++ ssh_host_resolver.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import queue
+import progressbar
 
 class SSHHostResolver:
 	def __init__(self, num_threads: int, data: dict):
@@ -8,6 +9,9 @@ class SSHHostResolver:
 		self.data = data
 		self.threads = []
 		self.q = queue.Queue()
+		self.count = len(data)
+		self.progress = progressbar.ProgressBar(max_value=self.count)
+		self.progress.update(0)
 
 		for host, props in data.items():
 			self.q.put_nowait(host)
@@ -27,6 +31,7 @@ class SSHHostResolver:
 		for t in self.threads:
 			t.join()
 
+		self.progress.finish()
 		return self.data
 
 	def __resolver_worker(self):
@@ -40,3 +45,5 @@ class SSHHostResolver:
 			self.data[host]["hostname"] = socket.getfqdn(host)
 			
 			self.q.task_done()
+			if (self.count - self.q.qsize()) % 100 == 0:
+				self.progress.update(self.count - self.q.qsize())
